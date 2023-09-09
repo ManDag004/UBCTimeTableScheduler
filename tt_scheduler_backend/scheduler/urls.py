5b159c6c166acfe8e3659eeb847f25f0039dff8a
Manan Daga <manandaga2004@gmail.com>
2023-09-08 18:21:44 -0700

    removed the unnecessary app from django project

--- tt_scheduler_backend/scheduler/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'scheduler'
-
-uripatterns = [
-  path('', TemplateView.as_view(template_name="scheduler/index.htmI")),
-]
