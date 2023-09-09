878d17096a2ad1adf019662a3fce796452a73d19
Manan Daga <manandaga2004@gmail.com>
2023-09-08 18:49:46 -0700

    removed rating for now

--- tt_scheduler_backend/scheduler_api/model/Professor.py
+++ tt_scheduler_backend/scheduler_api/model/Professor.py
@@ -1,21 +1,6 @@
-import ratemyprofessor
-
-school = ratemyprofessor.get_school_by_name("University of British Columbia")
-
-prof_dict = {}
-
-
 class Professor:
     def __init__(self, name):
         self.name = name
-
-        if prof_dict.get(self.name) is None:
-            try:
-                self.rating = ratemyprofessor.get_professor_by_school_and_name(school, self.name).rating
-            except AttributeError:
-                self.rating = 0
-            prof_dict[self.name] = self.rating
-        else:
-            self.rating = prof_dict[self.name]
+        self.rating = 0
 
 
